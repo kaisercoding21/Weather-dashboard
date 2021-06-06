412de15d9988e57c4a44cc3d3feffa1a50aef1d0
Kaisercoding <kaisercoding@outlook.com>
2021-06-06 15:24:07 -0400

    added fetch calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,55 +7,59 @@
 var userInput = document.querySelector("#searchForm");
 var userCityInput = document.querySelector("#city");
 
-// var userInput = "orlando";
+var displayCurrent = document.getElementById("#title");
 
-var currentWeather = fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=" +
-    userCity +
-    "&units=imperial&appid=6863e16fbd4761d2785095597280e28b"
-).then(function (response) {
-  response.json().then(function (currentData) {
-    console.log(currentData);
-  });
-});
-
-var futureCast = fetch(
-  "https://api.openweathermap.org/data/2.5/forecast?q=" +
-    userCity +
-    "&units=imperial&appid=6863e16fbd4761d2785095597280e28b"
-).then(function (response2) {
-  response2.json().then(function (futureData) {
-    console.log(futureData);
-  });
-});
+var currentWeather;
+var futureCast;
+userCity = "";
 
 //   function list
+
 var getUserInput = function (event) {
   event.preventDefault();
-  var userCity = userCityInput.value.trim().toLowerCase();
+  userCity = userCityInput.value.trim().toLowerCase();
   if (userCity) {
-    getCurrentWeather(userCity);
-    getFutureWeather(userCity);
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        userCity +
+        "&units=imperial&appid=6863e16fbd4761d2785095597280e28b"
+    ).then(function (response) {
+      response.json().then(function (currentData) {
+        // console.log(currentData);
+        currentWeather = currentData;
+        lat = currentData.coord.lat;
+        lon = currentData.coord.lon;
+        return fetch(
+          "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+            lat +
+            "&lon=" +
+            lon +
+            "&exclude=minutely,hourly,alerts&units=imperial&appid=6863e16fbd4761d2785095597280e28b"
+        ).then(function (response2) {
+          response2.json().then(function (futureData) {
+            futureCast = futureData;
+            console.log(futureData);
+            console.log("lat: " + lat + " longitude: " + lon);
+          });
+        });
+      });
+    });
+
+    timeout();
   }
-  console.log(cityTest);
+  // displayCurrentWeather();
 };
 
-var getCurrentWeather = function () {
-  fetch(currentWeather).then(function (response) {
-    response.json().then(function (temperature) {
-      console.log(temperature);
-    });
-  });
-};
-var getFutureWeather = function () {
-  fetch(futureCast).then(function (response) {
-    response.json().then(function (temperature) {
-      console.log(temperature);
-    });
-  });
-};
+// var displayCurrentWeather = function () {
+// };
+
 userInput.addEventListener("submit", getUserInput);
-
+var timeout = function () {
+  setTimeout(function () {
+    console.log(userCity);
+    console.log(currentWeather);
+    console.log(futureCast);
+    console.log(futureCast.daily[1].uvi);
+  }, 1000);
+};
 // // callbacks
-getCurrentWeather();
-getFutureWeather();
